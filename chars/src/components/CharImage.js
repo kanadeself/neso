340c3d0b46a6705c5dfdda645a12ec69b796b14b
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2022-11-23 21:00:29 +0100

    Sync

--- chars/src/components/CharImage.js
+++ chars/src/components/CharImage.js
@@ -1,7 +1,7 @@
 function CharImage(char){
     return (
         <div id={char.name} style={{padding: '8px'}}>
-            <a href={`${char.name.toLowerCase()}/`}>
+            <a href={`./?char=${char.name.toLowerCase()}`}>
                 <img src={`./img/${char.name.toLowerCase()}/portrait.png`} className="round-image btni" style={
                     {
                         backgroundColor: char.color,
--- chars/src/components/GenerateCharList.js
+++ chars/src/components/GenerateCharList.js
@@ -17,4 +17,3 @@ function RenderCharList(data){
     
     ReactDOM.render(<CharList />, document.getElementById('root'))
 }
-
