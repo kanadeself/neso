860acff8a9fd2d438b825d5b7925b12c6efda5f0
Jakub Holubansky <tortillaconalioli@gmail.com>
2022-12-11 16:53:16 +0100

    Prevent matching prefixes/suffixes when filtering by msize

--- chars/src/Utils/filter.js
+++ chars/src/Utils/filter.js
@@ -7,11 +7,13 @@ function filterSelection(c) {
 
     var x, i;
     x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
     for (i = 0; i < x.length; i++) {
         w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        // wrap class names in spaces to prevent matching prefixes or suffixes
+        if (c === "all" || (' ' + x[i].className + ' ').indexOf(' ' + c + ' ') > -1) {
+            w3AddClass(x[i], "show");
+        }
     }
 }
 
